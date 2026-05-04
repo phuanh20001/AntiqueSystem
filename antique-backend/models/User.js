e068b93da3ca5fff4e249eefa955c2bd5da6f303
arjunjham-coder <arjunjham@gmail.com>
2026-05-04 12:49:22 +0545

    add trim and lowercase to user schema fields

--- antique-backend/models/User.js
+++ antique-backend/models/User.js
@@ -8,12 +8,15 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, 'Please provide a username'],
-    unique: true
+    unique: true,
+    trim: true
   },
   email: {
     type: String,
     required: [true, 'Please provide an email'],
     unique: true,
+    lowercase: true,
+    trim: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'Please add a valid email'
