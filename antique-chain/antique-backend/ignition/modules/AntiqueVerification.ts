6dce406c282ea2920dd90a69e67610695540dfa3
Ngoc Phu Anh Nguyen <cihe231574@student.cihe.edu.au>
2026-05-06 19:58:17 +1000

    Reapply "Reapply "Merge branch 'main' of https://github.com/phuanh20001/AntiqueSystem""
    This reverts commit 90b4b2f8494b36264feb86d090154e63c3817a31.

--- antique-chain/antique-backend/ignition/modules/AntiqueVerification.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("AntiqueVerificationModule", (m) => {
-  const antiqueVerification = m.contract("AntiqueVerification");
-
-  return { antiqueVerification };
-});
